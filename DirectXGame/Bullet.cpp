75433306cb2ec1239c98d136485c246f24f0b28d
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-10-22 15:18:18 +0900

    弾の発射をどの方向に撃っても真っすぐとっで行けるようにした

--- DirectXGame/Bullet.cpp
+++ DirectXGame/Bullet.cpp
@@ -11,7 +11,9 @@ void Bullet::Initialize(Model* model, const Vector3& position, const Vector3& ve
 	// ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
-	worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f;
+
+	//弾の向き
+	worldTransform_.rotation_.y =std::atan2(velocity.x,velocity.z);
 
 	//発射方向へスピード設定
 	velocity_ = velocity*1.0f;
@@ -25,14 +27,18 @@ void Bullet::Initialize(Model* model, const Vector3& position, const Vector3& ve
 
 void Bullet::Update() { 
 	
+	//現在の速度ベクトル文だけ前進
 	worldTransform_.translation_ += velocity_;
 
 	lifeTime_--;
+
+	//寿命が尽きたら削除フラグON
 	if (lifeTime_ <= 0) {
 	
 		IsDead_ = true;
 	}
 
+	//行列を更新
 	WorldTransformUpdate(worldTransform_);
 }
 
--- DirectXGame/BulletManager.cpp
+++ DirectXGame/BulletManager.cpp
@@ -6,9 +6,12 @@ void BulletManager::Initialize(Model* model) {
 
 void BulletManager::Update() {
 
+	//弾のリストをループして更新
 	for (auto it = bullets_.begin(); it != bullets_.end();) {
 		Bullet* bullet = *it;
 		bullet->Update();
+
+		//死亡していたら削除
 		if (bullet->IsDead()) {
 			delete bullet;
 			it = bullets_.erase(it);
@@ -29,9 +32,13 @@ void BulletManager::Draw(Camera* camera) {
 }
 
 void BulletManager::Fire(const Vector3& position, const Vector3& direction) { 
+
+	//新しい弾を生成
 	Bullet* newBullet = new Bullet();
-	//速度倍率
+
+	//発射方向を速度として設定
 	Vector3 velocity = direction * 1.0f;
+
 	newBullet->Initialize(model_, position, velocity);
 	bullets_.push_back(newBullet);
 
