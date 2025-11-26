6d5d46a62018a06ff8eaf0c690d9b778332e7395
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-11-26 15:50:54 +0900

    ヘッダーにあったusing namespaceを削除

--- DirectXGame/FollowCamera.h
+++ DirectXGame/FollowCamera.h
@@ -5,23 +5,23 @@
 class FollowCamera {
 public:
 	// 初期化
-	void Initialize(Camera* camera);
+	void Initialize(KamataEngine::Camera* camera);
 
 	// 更新
 	void Update();
 
 	// プレイヤーを設定
-	void SetTarget(const Vector3* target) { target_ = target; }
-	void SetTargetRotation(const Vector3* targetRotation) { targetRotation_ = targetRotation; }
+	void SetTarget(const KamataEngine::Vector3* target) { target_ = target; }
+	void SetTargetRotation(const KamataEngine::Vector3* targetRotation) { targetRotation_ = targetRotation; }
 
 	// 注視オフセット（少し上を見せたい時など）
-	void SetTargetOffset(const Vector3& offset) { targetOffset_ = offset; }
+	void SetTargetOffset(const KamataEngine::Vector3& offset) { targetOffset_ = offset; }
 
 	// 距離
 	void SetDistance(float distance) { distance_ = distance; }
 
 	//ロックオンターゲット設定
-	void SetLockOnTarget(const Vector3* lockOnTarget) { lockOnTarget_ = lockOnTarget; }
+	void SetLockOnTarget(const KamataEngine::Vector3* lockOnTarget) { lockOnTarget_ = lockOnTarget; }
 
 	void SetLockOn(bool flag) { isLockOn_ = flag;}
 
@@ -30,31 +30,31 @@ public:
 
 private:
 	// プレイヤー位置へのポインタ
-	const Vector3* target_ = nullptr; 
+	const KamataEngine::Vector3* target_ = nullptr; 
 
-	const Vector3* targetRotation_ = nullptr;
+	const KamataEngine::Vector3* targetRotation_ = nullptr;
 
 	// 敵ターゲット
-	const Vector3* lockOnTarget_ = nullptr;
+	const KamataEngine::Vector3* lockOnTarget_ = nullptr;
 	
 	//ロックオン状態
 	bool isLockOn_ = false;
 
 	//注視店のオフセット
-	Vector3 targetOffset_ = {0.0f, 2.0f, 0.0f};
+	KamataEngine::Vector3 targetOffset_ = {0.0f, 2.0f, 0.0f};
 	//プレイヤーとの距離
 	float distance_ = 10.0f;
 	//カメラ
-	Camera *camera=nullptr;
+	KamataEngine::Camera* camera = nullptr;
 
 	//カメラの位置
-	Vector3 translation_ = {0, 0, 0};
+	KamataEngine::Vector3 translation_ = {0, 0, 0};
 
 	//カメラの回転
-	Vector3 rotation_ = {0, 0, 0};
+	KamataEngine::Vector3 rotation_ = {0, 0, 0};
 
 	//注視点
-	Vector3 lookAt_ = {0, 0, 0};
+	KamataEngine::Vector3 lookAt_ = {0, 0, 0};
 
 	
 };
--- DirectXGame/MyMath.h
+++ DirectXGame/MyMath.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "KamataEngine.h"
-using namespace KamataEngine;
+
 
 namespace KamataEngine {
 
@@ -20,25 +20,25 @@ Vector3 operator-(const Vector3& lhv, const Vector3& rhv);
 } // namespace KamataEngine
 
 // 平行移動
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+KamataEngine::Matrix4x4 MakeTranslateMatrix(const KamataEngine::Vector3& translate);
 
 // 拡大縮小
-Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+KamataEngine::Matrix4x4 MakeScaleMatrix(const KamataEngine::Vector3& scale);
 
 // X軸回転行列
-Matrix4x4 MakeRoteXMatrix(float radian);
+KamataEngine::Matrix4x4 MakeRoteXMatrix(float radian);
 
 // Y軸回転行列
-Matrix4x4 MakeRotateYMatrix(float radian);
+KamataEngine::Matrix4x4 MakeRotateYMatrix(float radian);
 
 // Z軸回転行列
-Matrix4x4 MakeRotateZMatrix(float radian);
+KamataEngine::Matrix4x4 MakeRotateZMatrix(float radian);
 
 // 積
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+KamataEngine::Matrix4x4 Multiply(const KamataEngine::Matrix4x4& m1, const KamataEngine::Matrix4x4& m2);
 
 // アフィン変換行列
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+KamataEngine::Matrix4x4 MakeAffineMatrix(const KamataEngine::Vector3& scale, const KamataEngine::Vector3& rotate, const KamataEngine::Vector3& translate);
 
 // 線形保管
 float Lerp(float a, float b, float t);
@@ -53,22 +53,22 @@ float EaseOut(float x1, float x2, float t);
 float EaseIn(float x1, float x2, float t);
 
 // 02_06のスライド24枚目のLerp関数
-Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);
+KamataEngine::Vector3 Lerp(const KamataEngine::Vector3& v1, const KamataEngine::Vector3& v2, float t);
 
 // 角度差を考慮した線形補間
 float LerpAngle(float a, float b, float t);
 
 // AABB関数
 struct AABB {
-	Vector3 min;
-	Vector3 max;
+	KamataEngine::Vector3 min;
+	KamataEngine::Vector3 max;
 };
 
 bool IsCollision(const AABB& aabb1, const AABB& aabb2);
 
 // トランスフォーム
-Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
-Vector3 TransformNormal(const Vector3& vector, const Matrix4x4& matrix);
+KamataEngine::Vector3 Transform(const KamataEngine::Vector3& vector, const KamataEngine::Matrix4x4& matrix);
+KamataEngine::Vector3 TransformNormal(const KamataEngine::Vector3& vector, const KamataEngine::Matrix4x4& matrix);
 
 // ワールドトランスフォーム更新
 void WorldTransformUpdate(KamataEngine::WorldTransform& worldTransform);
@@ -76,6 +76,6 @@ void WorldTransformUpdate(KamataEngine::WorldTransform& worldTransform);
 inline float ToRadians(float degrees) { return degrees * (3.1415f / 180.0f); }
 inline float ToDegrees(float radians) { return radians * (180.0f / 3.1415f); }
 
-float Length(const Vector3& v);
+float Length(const KamataEngine::Vector3& v);
 
-Vector3 Normalize(const Vector3& v);
+KamataEngine::Vector3 Normalize(const KamataEngine::Vector3& v);
